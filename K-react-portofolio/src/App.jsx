4e1abdab753b936a32b4765e7beda9eefee974d4
kaneganteng <kaneesasta@gmail.com>
2024-10-30 10:05:22 -0400

    changed footer file name to kakier

--- K-react-portofolio/src/App.jsx
+++ K-react-portofolio/src/App.jsx
@@ -2,7 +2,7 @@
 import { Outlet } from 'react-router-dom';
 // import './App.css'
 import Header from './components/header';
-import Footer from './components/Footer';
+import Footer from './components/kakier';
 
 function App() {
 
--- K-react-portofolio/src/components/kakier.jsx
+++ K-react-portofolio/src/components/kakier.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <footer className="container">
       footer
-      {/* <a href="https://github.com/kaneganteng" target="_blank" rel="noopener nooreferrer"><i className="fa-brands fa-github" /></a>
+      <a href="https://github.com/kaneganteng" target="_blank" rel="noopener nooreferrer"><i className="fa-brands fa-github" /></a>
       <a href="https://www.linkedin.com/in/kane-esasta-a03a282b2/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
-      <a href="https://www.instagram.com/kane.esasta/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-x-instagram"></i></a> */}
+      <a href="https://www.instagram.com/kane.esasta/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-x-instagram"></i>testkane</a>
     </footer>
   )
 }
