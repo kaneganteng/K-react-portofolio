231624ab67ef95f54979c47cd8f946adf3b31efd
kaneganteng <kaneesasta@gmail.com>
2024-10-30 01:48:19 -0400

    added changes to aboutme and contact.jsx with some styling

--- K-react-portofolio/src/pages/AboutMe.jsx
+++ K-react-portofolio/src/pages/AboutMe.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import myImage from '../asset/RainbowMountainKane.jpeg';
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className="container-fluid">
+        <h1>About Me</h1>
+        <img src={myImage} alt="Picture of Kane Esasta on the peak of Rainbow Mountain in Peru" width="300" height="400" />
+        <p>Hi! My name is Kane Esasta, a fresh student who enrolled in edX Boot Camps LLC for full stack development through University of Central Florid <span>&#40;</span>UCF<span>&#41;</span>.
+           I am passionate to learn and work in the coding field. I will earn my certificate after finishing this boot camp in december 2024.
+           Before going into the bootcamp, I went to Temple University in Philadelphia and earned my Bachelor of Science in Health Professions which is very unrelated to programming.
+           I enrolled myself in a bootcamp in the hope of learning something new and get a certificate while at it. Hopefully, after finishing this semester with the boot camp, I will gain the skills that I can use in the working field.
+           Certainly, feel free to reach out to me if you have something to teach me relating to code, job opportunities that I may be eligible for or even opportunities where I can get coding training on the job and hired after. 
+        </p>
+    </div>
   )
 }
 
--- K-react-portofolio/src/pages/Contact.jsx
+++ K-react-portofolio/src/pages/Contact.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Contact = () => {
+
+
   return (
-    <div>Contact</div>
+    <div className="container-md">
+      {/* title for Contact page */}
+      <h2>Contact</h2>
+      {/* Form for contact information that includes name, email, and some message for questions or comments */}
+      <form>
+        {/* Name and text-input section (required) */}
+        <div>
+          <label className="form-label">Name:</label>
+          <input type="text" className="form-control" required />
+        </div>
+        {/* Email Address and type email input (required and needs to be in an email format) */}
+        <div>
+          <label className="form-label">Email Address:</label>
+          <input type="email" className="form-control" aria-describedby="emailHelp" Required />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div>
+          <label className="form-label">Message:</label>
+          <input type="message" className="form-control" aria-describedby="emailHelp" Required />
+        </div>
+        <button type="submit" className="btn btn-primary">SUBMIT</button>
+      </form>
+    </div>
   )
 }
 
