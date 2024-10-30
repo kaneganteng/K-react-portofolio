2abc482e3acc552545f8dcd9af0819643885c20f
kaneganteng <kaneesasta@gmail.com>
2024-10-30 10:14:02 -0400

    install font awesome to the propject and changed footer.jsx back

--- K-react-portofolio/src/App.jsx
+++ K-react-portofolio/src/App.jsx
@@ -2,7 +2,7 @@
 import { Outlet } from 'react-router-dom';
 // import './App.css'
 import Header from './components/header';
-import Footer from './components/kakier';
+import Footer from './components/Footer';
 
 function App() {
 
